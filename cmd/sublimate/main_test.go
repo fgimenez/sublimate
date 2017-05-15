6f07a860ea91a38e243aee0709ea2cc340de9bae
Federico Gimenez <fgimenez@canonical.com>
2017-05-15 21:16:56 +0200

    back to build and using glide from travis

--- cmd/sublimate/main_test.go
+++ cmd/sublimate/main_test.go
@@ -8,16 +8,15 @@ import (
 	"testing"
 )
 
-var sublimateBin string
+const sublimateBin = "/tmp/sublimate"
 
 func TestMain(m *testing.M) {
 	// compile command/
-	cmd := exec.Command("go", "get", "./cmd/sublimate/")
+	cmd := exec.Command("go", "build", "-o", sublimateBin, "./cmd/sublimate/")
 	cmd.Dir = filepath.Join(os.Getenv("GOPATH"), "src/github.com/fgimenez/sublimate")
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("error building command %v", err)
 	}
 	defer os.Remove(sublimateBin)
-	sublimateBin = filepath.Join(os.Getenv("GOPATH"), "bin/sublimate")
 	os.Exit(m.Run())
 }

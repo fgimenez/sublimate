ca558444c9ac56371b679e8fae4783df6293ff9b
Federico Gimenez <fgimenez@canonical.com>
2017-05-15 18:42:15 +0200

    get instead of build

--- cmd/sublimate/main_test.go
+++ cmd/sublimate/main_test.go
@@ -8,15 +8,16 @@ import (
 	"testing"
 )
 
-const sublimateBin = "/tmp/sublimate"
+var sublimateBin string
 
 func TestMain(m *testing.M) {
 	// compile command/
-	cmd := exec.Command("go", "build", "-o", sublimateBin, "./cmd/sublimate/")
+	cmd := exec.Command("go", "get", "./cmd/sublimate/")
 	cmd.Dir = filepath.Join(os.Getenv("GOPATH"), "src/github.com/fgimenez/sublimate")
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("error building command %v", err)
 	}
 	defer os.Remove(sublimateBin)
+	sublimateBin = filepath.Join(os.Getenv("GOPATH"), "bin/sublimate")
 	os.Exit(m.Run())
 }
